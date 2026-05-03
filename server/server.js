cb6cbd65380f7f956c98d71b9e7a9692a99d20ac
Helal Uddin -SWE <seoguro1999@gmail.com>
2026-05-03 13:14:59 +0600

    intro to devcommerce

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express=require("express")
+const app=express()
+const PORT=5001
+
+app.get('/',(req,res)=>{
+res.send("Api is running")
+})
+app.listen(PORT,()=>{
+  console.log(`server running on http://localhost:${PORT}`)
+})
